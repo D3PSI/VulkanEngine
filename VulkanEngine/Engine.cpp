65831614653e316bb8e71f9fdaa1414d62b1860d
Unknown <cedricschwyter@bluewin.ch>
2018-12-28 08:57:41 +0100

    Fixed error

--- VulkanEngine/Engine.cpp
+++ VulkanEngine/Engine.cpp
@@ -2134,35 +2134,35 @@ void Engine::createDescriptorSetLayout(void) {
 
 	VkDescriptorSetLayoutBinding uboLayoutBinding				= {};
 	uboLayoutBinding.binding									= 0;
-	uboLayoutBinding.descriptorType								= VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	uboLayoutBinding.descriptorCount							= 1;
-	uboLayoutBinding.stageFlags									= VK_SHADER_STAGE_VERTEX_BIT;
+	uboLayoutBinding.descriptorType								= VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	uboLayoutBinding.pImmutableSamplers							= nullptr;
+	uboLayoutBinding.stageFlags									= VK_SHADER_STAGE_VERTEX_BIT;
 
 	VkDescriptorSetLayoutBinding samplerLayoutBinding			= {};
-	uboLayoutBinding.binding									= 1;
-	uboLayoutBinding.descriptorType								= VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-	uboLayoutBinding.descriptorCount							= 1;
-	uboLayoutBinding.stageFlags									= VK_SHADER_STAGE_FRAGMENT_BIT;
-	uboLayoutBinding.pImmutableSamplers							= nullptr;
+	samplerLayoutBinding.binding								= 1;
+	samplerLayoutBinding.descriptorCount						= 1;
+	samplerLayoutBinding.descriptorType							= VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+	samplerLayoutBinding.pImmutableSamplers						= nullptr;
+	samplerLayoutBinding.stageFlags								= VK_SHADER_STAGE_FRAGMENT_BIT;
 
-	std::array< VkDescriptorSetLayoutBinding, 2 > bindings		= {uboLayoutBinding, samplerLayoutBinding};
+	std::array<VkDescriptorSetLayoutBinding, 2> bindings		= { uboLayoutBinding, samplerLayoutBinding };
 	VkDescriptorSetLayoutCreateInfo layoutInfo					= {};
 	layoutInfo.sType											= VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
 	layoutInfo.bindingCount										= static_cast< uint32_t >(bindings.size());
 	layoutInfo.pBindings										= bindings.data();
 
 	if (vkCreateDescriptorSetLayout(
-	
-		device,
+		
+		device, 
 		&layoutInfo,
 		nullptr,
 		&descriptorSetLayout
 	
 	) != VK_SUCCESS) {
-	
+
 		logger.log(ERROR_LOG, "Failed to create descriptor set layout!");
-	
+
 	}
 
 }
@@ -2351,7 +2351,7 @@ void Engine::createTextureImage(void) {
 	int texWidth, texHeight, texChannels;
 	stbi_uc* pixels = stbi_load(
 	
-		"res/textures/texture.jpg",
+		"res/textures/texture.png",
 		&texWidth,
 		&texHeight,
 		&texChannels,
