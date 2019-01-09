7a36565cf2a72086950e9847e36ab66f21654b96
Unknown <cedricschwyter@bluewin.ch>
2019-01-09 12:07:02 +0100

    Enable development makro

--- VulkanEngine/VERSION.cpp
+++ VulkanEngine/VERSION.cpp
@@ -1,3 +1,3 @@
 #pragma once
-#define GAME_RELEASE		// #define GAME_RELEASE				for releasing software
-							// #define GAME_DEVELOPMENT			for developtment in IDE's
+// #define GAME_RELEASE				// for releasing software
+#define GAME_DEVELOPMENT			// for developtment in IDE's or testing on different systems (debugging)
