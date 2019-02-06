cfa429960279e5f723dc00ab21034447997eb758
Unknown <cedricschwyter@bluewin.ch>
2019-02-06 18:11:41 +0100

    Save, work in progress...

--- VulkanEngine/Engine.cpp
+++ VulkanEngine/Engine.cpp
@@ -1777,7 +1777,7 @@ void Engine::createCommandBuffers(void) {
 		renderPassBeginInfo.renderArea.extent			= swapChainExtent;
 
 		std::array< VkClearValue, 2 > clearValues		= {};
-		clearValues[0].color							= {122.0f / 255.0f, 122.0f / 255.0f, 122.0f / 255.0f, 1.0f};
+		clearValues[0].color							= {0.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 1.0f};
 		clearValues[1].depthStencil						= {1.0f, 0};
 		renderPassBeginInfo.clearValueCount				= static_cast< uint32_t >(clearValues.size());
 		renderPassBeginInfo.pClearValues				= clearValues.data();
